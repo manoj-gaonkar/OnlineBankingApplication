081cbb80532b33e2db47c2dfb8a1a68e3b0a89f0
manoj <manojgaonkar7648@gmail.com>
2023-07-21 00:52:19 +0530

    admin authentication

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -10,8 +10,14 @@ export class LoginService {
 
   constructor(private http:HttpClient) { }
 
-  loginUrl:string = "http://localhost:8080/api/v1.0/bank/login";
+  loginUrl:string | any;
   loginUser(loginUser:Login):Observable<any>{
+    this.loginUrl =  "http://localhost:8080/api/v1.0/bank/login/customer";
     return this.http.post(`${this.loginUrl}`,loginUser);
+  }
+
+  loginAdmin(loginUser:Login):Observable<any>{
+    this.loginUrl =  "http://localhost:8080/api/v1.0/bank/login/admin";
+    return this.http.post(`${this.loginUrl}`,loginUser)
   }
 }
